184199c8c518854c53216f606812a1b9a3aff6a3
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-09-15 19:52:56 -0400

    Update File Structure
    The code now saves each agency in its subfolder and unzips the downloaded files to then be used. Furthermore, created a function to download and unzip multiple files at once.

--- transit/Helpers/download_gtfs.py
+++ transit/Helpers/download_gtfs.py
@@ -1,6 +1,8 @@
+import os
 import ssl
 import urllib3
 import requests
+import zipfile
 from pathlib import Path
 from requests.adapters import HTTPAdapter
 from urllib3.util.ssl_ import create_urllib3_context
@@ -11,6 +13,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 GTFS_LINKS = {
     "grt_busses": "https://webapps.regionofwaterloo.ca/api/grt-routes/api/staticfeeds/1",
     "grt_trains": "https://webapps.regionofwaterloo.ca/api/grt-routes/api/staticfeeds/2",
+
+    "go"        : "https://assets.metrolinx.com/raw/upload/Documents/Metrolinx/Open%20Data/GO-GTFS.zip"
 }
 
 class WeakDHAdapter(HTTPAdapter):
@@ -34,7 +38,8 @@ class WeakDHAdapter(HTTPAdapter):
 
 
 def download_gtfs(agency:str):
-    local_path = "transit\\GTFS_Files"
+    local_path = Path("transit") / "GTFS_Files" / agency
+    local_path.mkdir(parents=True, exist_ok=True)
 
     session = requests.Session()
     session.mount("https://", WeakDHAdapter())
@@ -46,7 +51,21 @@ def download_gtfs(agency:str):
     response = session.get(url, verify=False)
 
     if response.status_code == 200:
-        with open(local_path + f"\\{agency}_data.zip", "wb") as file:
+        with open(local_path / "data.zip", "wb") as file:
             file.write(response.content)
+    else:
+        print(f"Failed to download GTFS data for {agency}, from {url}. Status code: {response.status_code}")
+        return -1
 
-download_gtfs("grt_busses")
+    with zipfile.ZipFile(local_path / "data.zip", 'r') as zip_ref:
+        zip_ref.extractall(local_path)
+
+    os.remove(local_path / "data.zip")
+
+    return 0
+
+def download_multiple_gtfs(*agencies:str):
+    for agency in agencies:
+        download_gtfs(agency)
+
+download_multiple_gtfs("go", "grt_busses", "grt_trains")

bb775ae42a0a6a4b4c7a3853c7920f2cc175c0de
kus <166449142+kus001@users.noreply.github.com>
2026-09-17 21:58:30 -0400

    now, we are able to take addresses and then find the duration and distance

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Map
 
-Map is a student-made open source routing software. Still under development.
+Map is a student-made open source routing software. Still under development. 
 
-Feedback welcome, please send to [host-transit-page@user.hackclub.app](mailto:host-transit-page@user.hackclub.app)
+Feedback welcome, please send to [host-transit-page@user.hackclub.app](mailto:host-transit-page@user.hackclub.app).
--- walking.py
+++ walking.py
@@ -1,20 +1,29 @@
 import requests
-from helpers.print_color import red, green, blue
+from helpers.print_color import red, green, blue, magenta
+from geopy.geocoders import Nominatim
 
-# get location
-startLat = float(input("Enter starting latitute: "))
-startLong = float(input("Enter starting longitute: "))
-endLat = float(input("Enter ending latitute: "))
-endLong = float(input("Enter ending longitude: "))
+geolocator = Nominatim(user_agent="map_walking_thirdspace")
 
-url = f"http://router.project-osrm.org/route/v1/foot/{startLong},{startLat};{endLong},{endLat}?overview=false"
+startingAddress = input("Enter starting address: ")
+startLocation = geolocator.geocode(startingAddress)
+startLat = startLocation.latitude
+startLong = startLocation.longitude
+
+endingAddress = input("Enter ending address: ")
+endLocation = geolocator.geocode(endingAddress)
+endLat = endLocation.latitude
+endLong = endLocation.longitude
+
+url = f"http://router.project-osrm.org/route/v1/walking/{startLong},{startLat};{endLong},{endLat}?overview=false"
 response = requests.get(url).json()
 
 distance = response['routes'][0]['distance']
-duration = response['routes'][0]['duration']
+duration = response['routes'][0]['duration'] # fix duration, the timing is wayyyy off
 
 print()
 print("Walking: ")
 print()
 print(blue(f"Distance: {distance / 1000:.2f} km"))
-print(f"Duration: {duration / 60:.2f} minutes")
+print(green(f"Duration: {duration / 60:.2f} minutes"))
+
+# add input detection ASAP

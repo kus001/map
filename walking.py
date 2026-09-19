c864c59c68dcac2868f7c7eb055662999e45f01e
kus <166449142+kus001@users.noreply.github.com>
2026-09-19 12:42:08 -0400

    added input detection to walking

--- walking.py
+++ walking.py
@@ -4,36 +4,38 @@ from geopy.geocoders import Nominatim
 
 geolocator = Nominatim(user_agent="map_walking_thirdspace")
 
-startingAddress = input("Enter starting address: ")
-startLocation = geolocator.geocode(startingAddress)
-startLat = startLocation.latitude
-startLong = startLocation.longitude
+while True: 
+    try: 
+        startingAddress = input("Enter starting address: ")
+        startLocation = geolocator.geocode(startingAddress)
+        startLat = startLocation.latitude
+        startLong = startLocation.longitude
 
-endingAddress = input("Enter ending address: ")
-endLocation = geolocator.geocode(endingAddress)
-endLat = endLocation.latitude
-endLong = endLocation.longitude
+        endingAddress = input("Enter ending address: ")
+        endLocation = geolocator.geocode(endingAddress)
+        endLat = endLocation.latitude
+        endLong = endLocation.longitude
 
-# url = f"http://router.project-osrm.org/route/v1/walking/{startLong},{startLat};{endLong},{endLat}?overview=false"
+        # url = f"http://router.project-osrm.org/route/v1/walking/{startLong},{startLat};{endLong},{endLat}?overview=false"
 
-url = (
-    f"https://host-transit-page.hackclub.app/route/v1/foot/"
-    f"{startLong},{startLat};"
-    f"{endLong},{endLat}"
-    f"?overview=false"
-)
+        url = (
+            f"https://host-transit-page.hackclub.app/route/v1/foot/"
+            f"{startLong},{startLat};"
+            f"{endLong},{endLat}"
+            f"?overview=false"
+        )
 
-response = requests.get(url).json()
+        response = requests.get(url).json()
 
-distance = response['routes'][0]['distance']
-duration = response['routes'][0]['duration'] # DURATION CALCULATION FIXED VIA SERVER CHANGE
+        distance = response['routes'][0]['distance']
+        duration = response['routes'][0]['duration'] # DURATION CALCULATION FIXED VIA SERVER CHANGE
 
-# (feedback from madhav) The duration is was off since it is calculating for the driving route. I have updated the API to use my custom server, which should fix it
+        # (feedback from madhav) The duration is was off since it is calculating for the driving route. I have updated the API to use my custom server, which should fix it
 
-print()
-print("Walking: ")
-print()
-print(blue(f"Distance: {distance / 1000:.2f} km"))
-print(green(f"Duration: {duration / 60:.2f} minutes"))
-
-# add input detection ASAP
+        print()
+        print("Walking: ")
+        print()
+        print(blue(f"Distance: {distance / 1000:.2f} km"))
+        print(green(f"Duration: {duration / 60:.2f} minutes"))
+    except AttributeError:
+        print(red("Enter valid address!"))

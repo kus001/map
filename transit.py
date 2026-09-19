a19f3e11146d8e0041af10d922c7ee76dff7f530
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-09-19 12:43:48 -0400

    Started Transit.py

--- transit.py
+++ transit.py
@@ -1,4 +1,11 @@
-import time
+import math
+from heapq import heappop, heappush
+from helpers.transit import make_graph
 from helpers.print_color import red, blue, bold, green
 
 print(bold(green("-------------------- Starting Transit Router --------------------")))
+
+data = make_graph()
+
+graph = data.graph
+stations = data.node_positions

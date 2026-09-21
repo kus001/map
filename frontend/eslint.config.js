173f5c9018142573d84e74a60a18adfab836e86f
Victor Jiao <160680951+BigBrain244466666@users.noreply.github.com>
2026-09-20 21:09:05 -0400

    Edit eslint.config.js
    Final Commit for week 1

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,21 +1,51 @@
+// Eslint.config.js
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  {
+    ignores:["dist", "node_modules"]
+  },
+
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      globals:globals.browser
     },
-  },
-])
+
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true
+        }
+      ],
+      "no-unused-vars": [
+        "error",
+        {
+          varsIgnorePattern: "^[A-Z_]",
+          argsIgnorePattern: "^_"
+        }
+      ]
+    }
+  }
+];
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,5 @@
+// Main.jsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 

ab0dbeacaad7a07389fd3aa06ed463d654585197
kus <166449142+kus001@users.noreply.github.com>
2026-09-25 17:51:13 -0400

    propely aligned the buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,20 +47,20 @@ function App() {
       </div>
 
       {/* buttons */}
-      <div className="w-60 grid grid-cols-4 p-4 mx-auto gap-0.5">
-        <button onClick={walking} className="relative flex items-center justify-center gap-4 mx-2.5 hover:shadow-lg hover:bg-button-darker mt-2.5 bg-button p-2 text-white rounded-lg">
+      <div className="w-60 grid grid-cols-4 mx-auto gap-2">
+        <button onClick={walking} className="relative flex items-center justify-center gap-4 hover:shadow-lg hover:bg-button-darker mt-2.5 bg-button p-2 text-white rounded-lg">
           <BsPersonWalking />
         </button>
 
-        <button onClick={cycling} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 hover:shadow-lg hover:bg-button-darker bg-button p-2 text-white rounded-lg">
+        <button onClick={cycling} className="relative flex items-center justify-center gap-4 mt-2.5 hover:shadow-lg hover:bg-button-darker bg-button p-2 text-white rounded-lg">
           <IoMdBicycle />
         </button>
 
-        <button onClick={driving} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 hover:shadow-lg hover:bg-button-darker bg-button p-2 text-white rounded-lg">
+        <button onClick={driving} className="relative flex items-center justify-center gap-4 mt-2.5 hover:shadow-lg hover:bg-button-darker bg-button p-2 text-white rounded-lg">
           <IoCarOutline />
         </button>
 
-        <button onClick={transit} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 hover:shadow-lg hover:bg-button-darker bg-button p-2 text-white rounded-lg">
+        <button onClick={transit} className="relative flex items-center justify-center gap-4 mt-2.5 hover:shadow-lg hover:bg-button-darker bg-button p-2 text-white rounded-lg">
           <MdDirectionsTransit />
         </button>
       </div>

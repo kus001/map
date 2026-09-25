502125ceabec215c8214360dd8778aa16d3d697d
kus <166449142+kus001@users.noreply.github.com>
2026-09-25 13:56:25 -0400

    added a proper grid layout for the buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,46 @@
 function App() {
   const walking = () => {
+    // when pressed, triggers get_walking_route(), shows the routes below the buttons, then updates the map to that route
     console.log("walking pressed")
   }
 
   const cycling = () => {
+    // same as above
     console.log("cycling pressed")
   }
 
   const driving = () => {
+    // same as above
     console.log("driving pressed")
   }
 
   const transit = () => {
+    // same as above
     console.log("transit pressed")
   }
 
   return (
-    <div className="w-screen h-screen">
-      <h1 className="text-3xl font-bold">
+    <div className="w-72 h-screen">
+      <h1 className="text-3xl font-bold text-center mt-2.5">
         Map Router
       </h1>
+      <div className="w-72 grid grid-cols-1 p-4 gap-1">
+        <button onClick={walking} className="relative mx-2.5 mt-2.5 bg-blue-500 p-2 text-white rounded-full">
+          Walking
+        </button>
 
-      <button onClick={walking} className="relative mx-2.5 mt-2.5 bg-blue-500 p-2 text-white rounded-full">
-        Walking
-      </button>
+        <button onClick={cycling} className="relative mx-2.5 mt-2.5 bg-green-500 p-2 text-white rounded-lg">
+          Cycling
+        </button>
 
-      <button onClick={cycling} className="relative mx-5 mt-2.5 bg-green-500 p-2 text-white rounded-lg">
-        Cycling
-      </button>
+        <button onClick={driving} className="relative mx-2.5 mt-2.5 bg-orange-500 p-2 text-white rounded-2xl">
+          Driving
+        </button>
 
-      <button onClick={driving} className="relative mx-2.5 mt-2.5 bg-orange-500 p-2 text-white rounded-2xl">
-        Driving
-      </button>
-
-      <button onClick={transit} className="relative mx-2.5 mt-2.5 bg-purple-500 p-2 text-white rounded-md">
-        Transit
-      </button>
+        <button onClick={transit} className="relative mx-2.5 mt-2.5 bg-purple-500 p-2 text-white rounded-md">
+          Transit
+        </button>
+      </div>
     </div>
   );
 }

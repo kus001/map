61d6d0ac7784ad21a9010c4a9476562e7fd7feec
kus <166449142+kus001@users.noreply.github.com>
2026-09-24 22:20:06 -0400

    react/tailwind website is up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <h1 className="text-3xl font-bold">
+        Map Router
+      </h1>
+    </div>
+  );
+}
+
+export default App;

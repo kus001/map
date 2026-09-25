c46c7221bbc8725c72dce3f1db188f9baf22c278
kus <166449142+kus001@users.noreply.github.com>
2026-09-25 16:07:37 -0400

    added hover animations on the buttons, and updated index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+// app.jsx
+
 // import SearchPanel from "./components/SearchPanel";
 import { IoMdBicycle } from "react-icons/io";
 import { IoCarOutline } from "react-icons/io5";
@@ -28,26 +30,28 @@ function App() {
   // next goal is to put all this into SearchPanel.jsx
   return (
     // the width of the left column is set to fixed for now, i want to get the layout and everything right and THEN make it flexible to different screen sizes
-    <div className="w-72 h-screen">
+    
+    // added a temporary border around all this, just so ik the area im working with 
+    <div className="w-72 h-screen border-button border-2"> 
       <h1 className="text-3xl font-bold text-center mt-2.5">
         Map Router
       </h1>
       <div className="w-72 grid grid-cols-1 p-4 gap-1">
-        <button onClick={walking} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 bg-[#757575] p-2 text-white rounded-lg">
+        <button onClick={walking} className="relative flex items-center justify-center gap-4 mx-2.5 hover:shadow-lg hover:bg-button/97 mt-2.5 bg-button p-2 text-white rounded-lg">
           <BsPersonWalking /> Walking
         </button>
 
-        <button onClick={cycling} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 bg-[#757575] p-2 text-white rounded-lg">
+        <button onClick={cycling} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 hover:shadow-lg hover:bg-button/97 bg-button p-2 text-white rounded-lg">
           <IoMdBicycle />
           Cycling
         </button>
 
-        <button onClick={driving} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 bg-[#757575] p-2 text-white rounded-lg">
+        <button onClick={driving} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 hover:shadow-lg hover:bg-button/97 bg-button p-2 text-white rounded-lg">
           <IoCarOutline />
           Driving
         </button>
 
-        <button onClick={transit} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 bg-[#757575] p-2 text-white rounded-lg">
+        <button onClick={transit} className="relative flex items-center justify-center gap-4 mx-2.5 mt-2.5 hover:shadow-lg hover:bg-buttom/97 bg-button p-2 text-white rounded-lg">
           <MdDirectionsTransit />
           Transit
         </button>

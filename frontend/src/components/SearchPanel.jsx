7c024dfeaddfdef7dd51a2501b07c76a18998ebb
Victor Jiao <160680951+BigBrain244466666@users.noreply.github.com>
2026-09-25 08:17:20 -0400

    Update SearchPanel.jsx

---
+++ frontend/src/components/SearchPanel.jsx
@@ -0,0 +1,26 @@
+import {
+    ArrowUpDown,
+    Bike,
+    Car,
+    Footprints,
+    LoaderCircle,
+    Search
+} from "lucid-react"
+
+const Modes = [
+    {
+        id:"driving",
+        label: "Drive",
+        icon: Car
+    },
+    {
+        id: "walking",
+        label: "Walk",
+        icon: Footprints
+    },
+    {
+        id: "Cycling",
+        label:"Bike",
+        icon: Bike
+    }
+];

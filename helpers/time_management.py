08d640c0c275b157e7d9093a849ecf64e735a1f8
roc-ket-cod-er <gargm0325@wrdsb.ca>
2026-09-25 20:03:00 -0400

    -

--- helpers/time_management.py
+++ helpers/time_management.py
@@ -17,3 +17,6 @@ def delta_time(time_str1, time_str2):
 def delta_time_in_minutes(time_str1, time_str2):
     dt = delta_time(time_str1, time_str2)
     return dt // 60, dt % 60
+
+if __name__ == '__main__':
+    print(time_to_seconds("15:00:00"))
